7d899c080d2f5829381d3b54e5dfdfd0a03b1a52
robson-sales <eng.robs@gmail.com>
2023-02-06 16:13:40 -0300

    Alterada chamada do buscaContatos

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,8 @@ const HomeModel = require('../models/HomeModel');
 const Contato = require('../models/ContatoModel');
 
 exports.index = async (req, res) => {
-    const contatos = await Contato.buscaContatos(req.session.user._id);
+    if(req.session.user._id){
+        const contatos = await Contato.buscaContatos(req.session.user._id);
+    }
     res.render('index', { title: 'Agenda | Home', contatos });
 };
